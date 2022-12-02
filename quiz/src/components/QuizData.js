8d61e379e98e3f6d0004275f1f582b06a44ec8c6
riyem <riyem.chebbi@gmail.com>
2022-12-02 07:18:39 +0100

    first commit

---
+++ quiz/src/components/QuizData.js
@@ -0,0 +1,39 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `"What is sex education?`,
+        options: [" Sex education is a way to teach children about abstinence.",
+         " Sex education does not need to include information about contraception.", 
+         "Sex education can only be taught to older students", 
+         ],
+        answer: "Sex education is a form of education that focuses on providing information about human sexual anatomy, reproduction, sexual intercourse, contraception, relationships, and sexual health"
+    },
+    {
+        id: 0,
+        question: "What is puberty?",
+        options: ["A type of flower", 
+        "A type of virus", 
+        "A type of bird",
+         ],
+        answer: "Puberty is the period of time during which physical and psychological changes occur in the body as one moves from childhood to adulthood."
+    },
+    {
+        id: 0,
+        question:  "What is the difference between gender and sex?",
+        options: [" There is no difference between gender and sex. ",
+        "Gender is a social construct and sex is biological.", 
+         
+          ],
+        answer: "Gender is the social, psychological, and cultural characterization of a person, while sex is the physical and biological characteristics of a person, such as male or female."
+    },
+    {
+        id: 0,
+        question: " What is the most effective form of contraception?",
+        options: ["Taking birth control pills", 
+        "Wearing a condom", 
+        "Using a diaphragm", 
+        ],
+        answer: "The most effective form of contraception is long-acting reversible contraception (LARC), such as the intrauterine device (IUD) and the contraceptive implant."
+    },
+     
+]
